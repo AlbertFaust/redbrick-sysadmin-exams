ae47f84b1142f246e216d31179373260780b634c
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-07-30 12:03:47 +0100

    Create wordpress.sh

---
+++ 2014/EGM/wordpress.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+while read user
+do
+  DIR=/home/$user/public_html
+  location=$(find $DIR -name wp-fucked.php)
+  if [ -n "$location" ]; then
+    rm -f $location
+    echo -e "A WordPress vulnerability has been found in your WordPress webtree.\nThe file 'wp-fucked.php' has been removed.\n" | mail -s "WordPress Vulner$
+  fi
+done < users.txt
