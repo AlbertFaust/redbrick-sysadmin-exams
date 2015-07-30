e82705a8da315a00ffa6834cbdc3a9d9be0b5dc9
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-07-30 12:17:35 +0100

    Update wordpress.sh
    Fixed copy paste error.

--- 2014/EGM/wordpress.sh
+++ 2014/EGM/wordpress.sh
@@ -5,6 +5,6 @@ do
   location=$(find $DIR -name wp-fucked.php)
   if [ -n "$location" ]; then
     rm -f $location
-    echo -e "A WordPress vulnerability has been found in your WordPress webtree.\nThe file 'wp-fucked.php' has been removed.\n" | mail -s "WordPress Vulner$
+    echo -e "A WordPress vulnerability has been found in your WordPress webtree.\nThe file 'wp-fucked.php' has been removed.\n" | mail -s "WordPress Vulnerability" "$user@redbrick.dcu.ie"
   fi
 done < users.txt
